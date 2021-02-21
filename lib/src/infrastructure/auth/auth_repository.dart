002ef7b497b4f3249ce37586cf93aed8c00e8d41
Yaicel Torres <yaiceltg@gmail.com>
2021-02-21 18:57:19 -0500

    lib/src/infrastructure/auth/auth_repository:

--- lib/src/infrastructure/auth/auth_repository.dart
+++ lib/src/infrastructure/auth/auth_repository.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:live4tsdk/src/domain/auth/auth_token.dart';
 import 'package:live4tsdk/src/domain/auth/auth_failure.dart';
 import 'package:live4tsdk/src/domain/auth/i_auth_repository.dart';
@@ -9,33 +10,87 @@ import 'package:live4tsdk/src/domain/auth/value_objects.dart';
 import 'package:live4tsdk/src/infrastructure/auth/auth_token_dto.dart';
 
 class AuthRepository implements IAuthRepository {
+  // http client
   Dio _httpClient;
+
+  // url routes
+  final String _authLoginPath = '/v1/auth/login';
+  final String _forgotPasswordPath = '/v1/auth/forgot-password';
+  final String _resetPasswordPath = '/v1/auth/reset-password';
 
   AuthRepository(this._httpClient);
 
   @override
-  Future<Either<AuthFailure, Unit>> forgotPassword({
-    EmailAddress emailAddress
-  }) {
-    // TODO: implement forgotPassword
-    throw UnimplementedError();
+  Future<Either<AuthFailure, Unit>> forgotPassword(
+      {@required EmailAddress emailAddress}) async {
+    try {
+      // prepare form data
+      final _data = jsonEncode({
+        'email': emailAddress.getOrCrash(),
+      });
+
+      // call api service
+      final _response = await _httpClient.post(
+        _forgotPasswordPath,
+        data: _data,
+      );
+
+      // check response
+      dynamic _responseData = _response.data;
+
+      if (_responseData is Map<String, dynamic> &&
+          _responseData.containsKey('message')) {
+        final String _message = _responseData['message'];
+
+        if (_message.contains('USER_NOT_FOUND')) {
+          return left(AuthFailure.userNotFound());
+        }
+      }
+
+      return right(unit);
+    } catch (e) {
+      return left(AuthFailure.serverError());
+    }
   }
 
   @override
-  Future<Either<AuthFailure, Unit>> resetPassword() {
-    // TODO: implement resetPassword
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<Either<AuthFailure, AuthToken>> signInWithEmailAndPassword({
-    EmailAddress emailAddress,
-    Password password
+  Future<Either<AuthFailure, Unit>> resetPassword({
+    @required ResetPasswordCode code,
+    @required Password password,
   }) async {
-     // define method path
-    final _path = '/v1/auth/login';
+    try {
+      // prepare form data
+      final _data = jsonEncode({
+        'code': code.getOrCrash(),
+        'password': password.getOrCrash(),
+      });
 
-    // call api
+      // call api service
+      final _response = await _httpClient.post(
+        _forgotPasswordPath,
+        data: _data,
+      );
+
+      // check response
+      Map<String, dynamic> _responseData = _response.data;
+      if (_responseData.containsKey('message')) {
+        final String _message = _responseData['message'];
+
+        if (_message.contains('USER_NOT_FOUND')) {
+          return left(AuthFailure.userNotFound());
+        }
+      }
+
+      return right(unit);
+    } catch (e) {
+      return left(AuthFailure.serverError());
+    }
+  }
+
+  @override
+  Future<Either<AuthFailure, AuthToken>> signInWithEmailAndPassword(
+      {@required EmailAddress emailAddress,
+      @required Password password}) async {
     try {
       // prepare form data
       final _data = jsonEncode({
@@ -45,22 +100,29 @@ class AuthRepository implements IAuthRepository {
 
       // call api service
       final _response = await _httpClient.post(
-        _path,
+        _authLoginPath,
         data: _data,
       );
 
       // check response
-      Map<String, dynamic> _responseData  = _response.data;
-      if(_responseData.containsKey('statusCode')) {
-        switch(_responseData['statusCode']) {
-          case 404:
+      Map<String, dynamic> _d = _response.data;
+      if (_d.containsKey('response')) {
+        final Map<String, dynamic> _r = _d['response'];
+
+        if (_r.containsKey('code')) {
+          String _c = _r['code'];
+
+          if (_c.contains('USER_NOT_FOUND')) {
             return left(AuthFailure.userNotFound());
-          default:
-            break;
+          }
+
+          if (_c.contains('INVALID_CREDENTIALS')) {
+            return left(AuthFailure.invalidEmailAndPasswordCombination());
+          }
         }
       }
 
-      final _token = AuthTokenDto.fromJson(_responseData).toDomain();
+      final _token = AuthTokenDto.fromJson(_d).toDomain();
 
       return right(_token);
     } catch (e) {
