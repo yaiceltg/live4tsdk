d5e4aed9dcce8102ef877ab2f8d87df990ac3937
Yaicel Torres <yaiceltg@gmail.com>
2021-02-21 19:04:33 -0500

    example/live4tsdk_example:

--- example/live4tsdk_example.dart
+++ example/live4tsdk_example.dart
@@ -2,22 +2,26 @@ import 'package:live4tsdk/live4tsdk.dart';
 import 'package:live4tsdk/src/domain/auth/value_objects.dart';
 
 void main() async {
+  // add global this client
   var live4tsdk = Live4tsdk(
     baseUrl: "http://localhost:3000/api"
   );
-  final _tokenOrError = await live4tsdk.auth.signInWithEmailAndPassword(
-    emailAddress: EmailAddress("mildredfigueroaq+1@gmail.com"),
-    password:  Password("a")
+
+  // call auth login service
+  final _tokenOrError = await live4tsdk.auth.forgotPassword(
+    emailAddress: EmailAddress("yaiceltg1@gmail.com"),
+    // password:  Password("a")
   );
 
 _tokenOrError.fold((error) {
-  print(
-    error.map(
-      serverError: (_) => "Server error ",
-      invalidEmailAndPasswordCombination: (_) => "Usuario o contraseña incorrecta",
-      userNotFound: (_) => "User not found",
-    )
+  final _eName = error.maybeMap(
+    serverError: (_) => "Server error ",
+    invalidEmailAndPasswordCombination: (_) => "Usuario o contraseña incorrecta",
+    userNotFound: (_) => "User not found",
+    orElse: () => "Ninguno"
   );
+
+  print(_eName);
 }, (token) {
   print("Token $token");
 });
