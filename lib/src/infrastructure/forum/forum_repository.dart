040f027d414c9b3f9c95d859ac091130d26709e6
Yaicel Torres <yaiceltg@gmail.com>
2021-03-21 18:59:42 -0400

    lib/src/infrastructure/forum/forum_repository:

--- lib/src/infrastructure/forum/forum_repository.dart
+++ lib/src/infrastructure/forum/forum_repository.dart
@@ -1,9 +1,11 @@
 import 'package:dio/dio.dart';
 import 'package:live4tsdk/src/domain/core/paged_list.dart';
+import 'package:live4tsdk/src/domain/forum/answer.dart';
 import 'package:live4tsdk/src/domain/forum/forum_failure.dart';
 import 'package:dartz/dartz.dart';
 import 'package:live4tsdk/src/domain/forum/i_forum_repository.dart';
 import 'package:live4tsdk/src/domain/forum/question.dart';
+import 'package:live4tsdk/src/infrastructure/forum/answer_dto.dart';
 import 'dart:convert';
 import '../../domain/forum/forum_failure.dart';
 import '../../domain/forum/question.dart';
@@ -16,6 +18,7 @@ class ForumRepository implements IForumRepository {
   // question url
   final String _createQuestionPath = '/v1/forum/questions';
   final String _fetchQuestionsPath = '/v1/forum/questions';
+  final String _addAnswerToQuestionsPath = '/v1/forum/questions/:questionId';
   // answer url
 
   ForumRepository(this._httpClient);
@@ -99,6 +102,77 @@ class ForumRepository implements IForumRepository {
           .toList();
 
       return right(PagedList(count: _count, items: _items));
+    } catch (e) {
+      return left(ForumFailure.serverError());
+    }
+  }
+
+  @override
+  Future<Either<ForumFailure, Unit>> addAnswer({required String questionId, required String body}) async{
+    try {
+      // prepare form data
+      final _data = jsonEncode({
+        'questionId': questionId,
+        'body': body,
+      });
+
+      // call api service
+      final response = await _httpClient!.post(
+        _addAnswerToQuestionsPath.replaceAll(":questionId'", questionId),
+        data: _data
+      );
+      // check response
+      if (response.data is Map<String, dynamic>) {
+        Map<String, dynamic> _d = response.data;
+
+        if (_d.containsKey('response')) {
+          final Map<String, dynamic> _r = _d['response'];
+
+          if (_r.containsKey('code')) {
+            String _c = _r['code'];
+
+            if (_c.contains('NOT_FOUND')) {
+              return left(ForumFailure.questionNotFound());
+            }
+          }
+        }
+      }
+
+      return right(unit);
+    } catch (e) {
+      return left(ForumFailure.serverError());
+    }
+  }
+
+  @override
+  Future<Either<ForumFailure, PagedList<Answer>>> fetchAnswers({required String questionId}) async {
+    try {
+      // call api service
+      final response = await _httpClient!.get(
+        _addAnswerToQuestionsPath.replaceAll(":questionId'", questionId),
+      );
+      // check response
+      if (response.data is Map<String, dynamic>) {
+        Map<String, dynamic> _d = response.data;
+
+        if (_d.containsKey('response')) {
+          final Map<String, dynamic> _r = _d['response'];
+
+          if (_r.containsKey('code')) {
+            String _c = _r['code'];
+
+          }
+        }
+      }
+
+      int _count = response.data['count'];
+
+      final _items = (response.data['items'] as List)
+          .map((e) => AnswerDto.fromJson(e).toDomain())
+          .toList();
+
+      return right(PagedList(count: _count, items: _items));
+
     } catch (e) {
       return left(ForumFailure.serverError());
     }
