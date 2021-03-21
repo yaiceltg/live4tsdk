03d7f685f7b32863227f1eb408d6c500ce7cd67b
Yaicel Torres <yaiceltg@gmail.com>
2021-03-21 18:59:47 -0400

    lib/src/domain/message/i_message_repository:

--- lib/src/domain/message/i_message_repository.dart
+++ lib/src/domain/message/i_message_repository.dart
@@ -1,1 +1,20 @@
-abstract class IMessageRepository {}
+import 'package:dartz/dartz.dart';
+import 'package:live4tsdk/src/domain/core/paged_list.dart';
+import 'package:live4tsdk/src/domain/message/message.dart';
+import 'package:live4tsdk/src/domain/message/message_failure.dart';
+
+abstract class IMessageRepository {
+  ///
+  /// function to send messaje
+  Future<Either<MessageFailure,Unit>> sendMessage({
+    required String to,
+    required String subject,
+    required String body
+  });
+
+
+  ///
+  /// fetch user messages
+  ///
+  Future<Either<PagedList<Message>,Unit>> fetchMessages();
+}
