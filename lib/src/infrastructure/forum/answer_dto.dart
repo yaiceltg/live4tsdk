74b4eb5b748b0be27236ce8a4c206c64c8284c58
Yaicel Torres <yaiceltg@gmail.com>
2021-03-21 18:59:34 -0400

    lib/src/infrastructure/forum/answer_dto:

--- lib/src/infrastructure/forum/answer_dto.dart
+++ lib/src/infrastructure/forum/answer_dto.dart
@@ -8,15 +8,40 @@ part 'answer_dto.g.dart';
 abstract class AnswerDto implements _$AnswerDto {
   const AnswerDto._();
 
-  const factory AnswerDto() = _AnswerDto;
+  const factory AnswerDto({
+    String? id,
+    String? queId,
+    String? userId,
+    String? body,
+    int? votesCount,
+    DateTime? createdAt,
+    DateTime? updatedAt,
+    String? bestAnswer
+  }) = _AnswerDto;
 
   factory AnswerDto.fromDomain(Answer answer) {
     return AnswerDto(
+      id: answer.id,
+      queId: answer.queId,
+      userId: answer.userId,
+      body: answer.body,
+      votesCount: answer.votesCount,
+      createdAt: answer.createdAt,
+      updatedAt: answer.updatedAt,
+      bestAnswer: answer.bestAnswer
     );
   }
 
   Answer toDomain() {
     return Answer(
+      id: id,
+      queId: queId,
+      userId: userId,
+      body: body,
+      votesCount: votesCount,
+      createdAt: createdAt,
+      updatedAt: updatedAt,
+      bestAnswer: bestAnswer
     );
   }
 
