7ad6c20a7da5f335c5a6ed5cbf1e66e095c7c055
Yaicel Torres <yaiceltg@gmail.com>
2021-03-20 23:40:26 -0400

    lib/src/domain/forum/i_forum_repository:

--- lib/src/domain/forum/i_forum_repository.dart
+++ lib/src/domain/forum/i_forum_repository.dart
@@ -5,7 +5,25 @@ import 'package:live4tsdk/src/domain/forum/question.dart';
 
 abstract class IForumRepository {
   Future<Either<ForumFailure, PagedList<Question>>> fetchQuestions();
+
   Future<Either<ForumFailure,Question>> fetchQuestionById(String questionId);
+
   Future<Either<ForumFailure,Unit>> createQuestion(Question question);
+
   Future<Either<ForumFailure,Unit>> updateQuestion(Question question);
+
+  ///
+  /// Serviceto add answer to question
+  ///
+  Future<Either<ForumFailure,Unit>> addAnswer({
+    required String questionId,
+    required String body,
+  });
+
+  ///
+  /// Service to get answers of question
+  ///
+  Future<Either<ForumFailure, PagedList<Question>>> fetchAnswers({
+    required String questionId
+  });
 }
