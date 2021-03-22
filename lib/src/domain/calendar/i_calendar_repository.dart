da744b23f102056daedc615f5e791ab2cc11a700
Yaicel Torres <yaiceltg@gmail.com>
2021-03-21 20:44:50 -0400

    lib/src/domain/calendar/i_calendar_repository:

--- lib/src/domain/calendar/i_calendar_repository.dart
+++ lib/src/domain/calendar/i_calendar_repository.dart
@@ -1,1 +1,32 @@
-abstract class ICalendarRepository {}
+import 'package:dartz/dartz.dart';
+import 'package:live4tsdk/src/domain/calendar/calendar_event.dart';
+import 'package:live4tsdk/src/domain/calendar/calendar_failure.dart';
+import 'package:live4tsdk/src/domain/core/paged_list.dart';
+
+abstract class ICalendarRepository {
+  ///
+  /// list calendar events
+  ///
+  Future<Either<CalendarFailure, PagedList<CalendarEvent>>> fetchEvents();
+
+  ///
+  /// create calendar event
+  ///
+  Future<Either<CalendarFailure, Unit>> createEvent({
+    required String title
+  });
+
+  ///
+  /// update calendar event
+  ///
+  Future<Either<CalendarFailure, Unit>> updateEvent({
+    required CalendarEvent event,
+  });
+
+  ///
+  /// delete calendar event
+  ///
+  Future<Either<CalendarFailure, Unit>> deleteEvent({
+    required CalendarEvent event
+  });
+}
