f92ad7229cd677ce87638b9f751fe807bf05e594
Yaicel Torres <yaiceltg@gmail.com>
2021-03-21 21:28:53 -0400

    lib/src/domain/chat/chat_group.freezed: create

---
+++ lib/src/domain/chat/chat_group.freezed.dart
@@ -0,0 +1,82 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides
+
+part of 'chat_group.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+class _$ChatGroupTearOff {
+  const _$ChatGroupTearOff();
+
+  _Message call() {
+    return const _Message();
+  }
+}
+
+/// @nodoc
+const $ChatGroup = _$ChatGroupTearOff();
+
+/// @nodoc
+mixin _$ChatGroup {}
+
+/// @nodoc
+abstract class $ChatGroupCopyWith<$Res> {
+  factory $ChatGroupCopyWith(ChatGroup value, $Res Function(ChatGroup) then) =
+      _$ChatGroupCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class _$ChatGroupCopyWithImpl<$Res> implements $ChatGroupCopyWith<$Res> {
+  _$ChatGroupCopyWithImpl(this._value, this._then);
+
+  final ChatGroup _value;
+  // ignore: unused_field
+  final $Res Function(ChatGroup) _then;
+}
+
+/// @nodoc
+abstract class _$MessageCopyWith<$Res> {
+  factory _$MessageCopyWith(_Message value, $Res Function(_Message) then) =
+      __$MessageCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$MessageCopyWithImpl<$Res> extends _$ChatGroupCopyWithImpl<$Res>
+    implements _$MessageCopyWith<$Res> {
+  __$MessageCopyWithImpl(_Message _value, $Res Function(_Message) _then)
+      : super(_value, (v) => _then(v as _Message));
+
+  @override
+  _Message get _value => super._value as _Message;
+}
+
+/// @nodoc
+class _$_Message extends _Message {
+  const _$_Message() : super._();
+
+  @override
+  String toString() {
+    return 'ChatGroup()';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) || (other is _Message);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+}
+
+abstract class _Message extends ChatGroup {
+  const factory _Message() = _$_Message;
+  const _Message._() : super._();
+}
