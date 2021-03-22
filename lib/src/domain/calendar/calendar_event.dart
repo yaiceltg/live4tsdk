8c659e7515764f3804ffc0044bf0d75faa29fd2c
Yaicel Torres <yaiceltg@gmail.com>
2021-03-21 21:33:02 -0400

    lib/src/domain/calendar/calendar_event:

--- lib/src/domain/calendar/calendar_event.dart
+++ lib/src/domain/calendar/calendar_event.dart
@@ -3,7 +3,9 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 part 'calendar_event.freezed.dart';
 
 @freezed
-abstract class CalendarEvent with _$CalendarEvent {
+class CalendarEvent with _$CalendarEvent {
+  const CalendarEvent._();
+
   const factory CalendarEvent(
   ) = _CalendarEvent;
 }
