c133302df031168e1dd6d136b5f5e3adb874f106
Yaicel Torres <yaiceltg@gmail.com>
2021-03-21 21:33:05 -0400

    lib/src/domain/calendar/calendar_failure:

--- lib/src/domain/calendar/calendar_failure.dart
+++ lib/src/domain/calendar/calendar_failure.dart
@@ -3,6 +3,6 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 part 'calendar_failure.freezed.dart';
 
 @freezed
-abstract class CalendarFailure with _$CalendarFailure {
+class CalendarFailure with _$CalendarFailure {
   const factory CalendarFailure.serverError() = _ServerError;
 }
