3ffe9b05421ddb32b8e556ecf61619bee35b2ad9
Yaicel Torres <yaiceltg@gmail.com>
2021-03-21 21:46:49 -0400

    update

--- lib/src/domain/scheduler/i_scheduler_repository.dart
+++ lib/src/domain/scheduler/i_scheduler_repository.dart
@@ -1,3 +1,9 @@
+import 'package:dartz/dartz.dart';
+import 'package:live4tsdk/src/domain/core/paged_list.dart';
+
 abstract class ISchedulerRepository {
-  // listar
+  ///
+  /// fetch scheduler
+  ///
+  // Future<Either<MessageFailure, PagedList<Message>>> fetchMessages();
 }
