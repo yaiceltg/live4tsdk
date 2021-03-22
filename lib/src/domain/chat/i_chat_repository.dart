8d6632d12bb3686812ea17a87ada75880caa8b89
Yaicel Torres <yaiceltg@gmail.com>
2021-03-21 21:27:47 -0400

    lib/src/domain/chat/i_chat_repository:

--- lib/src/domain/chat/i_chat_repository.dart
+++ lib/src/domain/chat/i_chat_repository.dart
@@ -1,1 +1,29 @@
-abstract class IChatRepository {}
+import 'package:dartz/dartz.dart';
+import 'package:live4tsdk/src/domain/chat/chat_failure.dart';
+import 'package:live4tsdk/src/domain/chat/chat_group.dart';
+import 'package:live4tsdk/src/domain/chat/chat_message.dart';
+import 'package:live4tsdk/src/domain/core/paged_list.dart';
+
+abstract class IChatRepository {
+  ///
+  /// fetch user chats
+  ///
+  Future<Either<ChatFailure, PagedList<ChatGroup>>> fetchGroups({
+    required String groupId,
+  });
+
+  ///
+  /// fetch group message
+  ///
+  Future<Either<ChatFailure, PagedList<ChatMessage>>> fetchMessage({
+    required String groupId,
+  });
+
+  ///
+  /// send chat message
+  ///
+  Future<Either<ChatFailure, Unit>> sendMessage({
+    required String to,
+    required String message
+  });
+}
