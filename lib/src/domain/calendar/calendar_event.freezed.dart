e77199a310b27e33048a77d4841431dd70206e06
Yaicel Torres <yaiceltg@gmail.com>
2021-03-21 20:44:44 -0400

    lib/src/domain/calendar/calendar_event.freezed: create

---
+++ lib/src/domain/calendar/calendar_event.freezed.dart
@@ -0,0 +1,86 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides
+
+part of 'calendar_event.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+class _$CalendarEventTearOff {
+  const _$CalendarEventTearOff();
+
+  _CalendarEvent call() {
+    return const _CalendarEvent();
+  }
+}
+
+/// @nodoc
+const $CalendarEvent = _$CalendarEventTearOff();
+
+/// @nodoc
+mixin _$CalendarEvent {}
+
+/// @nodoc
+abstract class $CalendarEventCopyWith<$Res> {
+  factory $CalendarEventCopyWith(
+          CalendarEvent value, $Res Function(CalendarEvent) then) =
+      _$CalendarEventCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class _$CalendarEventCopyWithImpl<$Res>
+    implements $CalendarEventCopyWith<$Res> {
+  _$CalendarEventCopyWithImpl(this._value, this._then);
+
+  final CalendarEvent _value;
+  // ignore: unused_field
+  final $Res Function(CalendarEvent) _then;
+}
+
+/// @nodoc
+abstract class _$CalendarEventCopyWith<$Res> {
+  factory _$CalendarEventCopyWith(
+          _CalendarEvent value, $Res Function(_CalendarEvent) then) =
+      __$CalendarEventCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$CalendarEventCopyWithImpl<$Res>
+    extends _$CalendarEventCopyWithImpl<$Res>
+    implements _$CalendarEventCopyWith<$Res> {
+  __$CalendarEventCopyWithImpl(
+      _CalendarEvent _value, $Res Function(_CalendarEvent) _then)
+      : super(_value, (v) => _then(v as _CalendarEvent));
+
+  @override
+  _CalendarEvent get _value => super._value as _CalendarEvent;
+}
+
+/// @nodoc
+class _$_CalendarEvent implements _CalendarEvent {
+  const _$_CalendarEvent();
+
+  @override
+  String toString() {
+    return 'CalendarEvent()';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) || (other is _CalendarEvent);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+}
+
+abstract class _CalendarEvent implements CalendarEvent {
+  const factory _CalendarEvent() = _$_CalendarEvent;
+}
