f62963a57bea422185d335a9c977df1ad4d89840
Yaicel Torres <yaiceltg@gmail.com>
2021-03-21 21:33:13 -0400

    lib/src/domain/message/message:

--- lib/src/domain/message/message.dart
+++ lib/src/domain/message/message.dart
@@ -3,7 +3,7 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 part 'message.freezed.dart';
 
 @freezed
-abstract class Message with _$Message {
+class Message with _$Message {
   const Message._();
 
   const factory Message(
