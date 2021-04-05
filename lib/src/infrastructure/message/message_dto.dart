cd03c332a1b77b272d9a20a9409eb234234a3bee
Yaicel Torres <yaiceltg@gmail.com>
2021-04-05 14:10:19 -0400

    lib/src/infrastructure/message/message_dto:

--- lib/src/infrastructure/message/message_dto.dart
+++ lib/src/infrastructure/message/message_dto.dart
@@ -1,48 +1,51 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:live4tsdk/src/domain/account/account.dart';
 import 'package:live4tsdk/src/domain/message/message.dart';
+import 'package:live4tsdk/src/infrastructure/account/account_dto.dart';
 
 part 'message_dto.freezed.dart';
 part 'message_dto.g.dart';
+
+AccountDto _from(args) {
+  return AccountDto.fromJson(args);
+}
 
 @freezed
 abstract class MessageDto implements _$MessageDto {
   const MessageDto._();
 
   const factory MessageDto({
-    required int id,
-    String? body,
+    required String id,
+    required AccountDto from, // @JsonKey(fromJson: _from)
+    required String subject,
+    required String body,
     DateTime? delivered,
     DateTime? read,
-    required int from,
-    required int to,
-    String? subject,
-    required DateTime createdAt,
-    required DateTime updatedAt,
+    DateTime? createdAt,
+    DateTime? updatedAt
   }) = _MessageDto;
 
   factory MessageDto.fromDomain(Message message) {
     return MessageDto(
-        id: message.id,
-        body: message.body,
-        delivered: message.delivered,
-        read: message.read,
-        from: message.from,
-        to: message.to,
-        subject: message.subject,
-        createdAt: message.createdAt,
-        updatedAt: message.updatedAt,
+      id: message.id,
+      from: AccountDto.fromDomain(message.from),
+      subject: message.subject,
+      body: message.body,
+      delivered: message.delivered,
+      read: message.read,
+      createdAt: message.createdAt,
+      updatedAt: message.updatedAt,
     );
   }
 
   Message toDomain() {
     return Message(
       id: id,
+      from: from.toDomain(),
+      subject: subject,
       body: body,
       delivered: delivered,
       read: read,
-      from: from,
-      to: to,
-      subject: subject,
       createdAt: createdAt,
       updatedAt: updatedAt,
     );
