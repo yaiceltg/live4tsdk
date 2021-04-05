309759f6d3de897197d32a1be50f91f65a8720af
Yaicel Torres <yaiceltg@gmail.com>
2021-04-05 14:29:33 -0400

    lib/src/live4tsdk_base:

--- lib/src/live4tsdk_base.dart
+++ lib/src/live4tsdk_base.dart
@@ -2,20 +2,43 @@ import 'package:dio/dio.dart';
 import 'package:live4tsdk/src/infrastructure/account/account_repository.dart';
 import 'package:live4tsdk/src/infrastructure/auth/auth_repository.dart';
 import 'package:live4tsdk/src/infrastructure/calendar/calendar_repository.dart';
+import 'package:live4tsdk/src/infrastructure/chat/chat_repository.dart';
 import 'package:live4tsdk/src/infrastructure/forum/forum_repository.dart';
 import 'package:live4tsdk/src/infrastructure/message/message_repository.dart';
 
 class Live4tsdk {
+
+  ///
+  /// Dio http client config
+  ///
+  // final String baseUrl;
   Dio? httpClient;
 
-
+  ///
+  /// Api repository
+  ///
   late AccountRepository account;
   late AuthRepository auth;
   late CalendarRepository calendar;
-  // late ChatRepository chat;
+  late ChatRepository chat;
   late ForumRepository forum;
   late MessageRepository message;
   // late SchedulerRepository scheduler;
+
+  ///
+  /// _cache is library-private, thanks to
+  /// the _ in front of its name.
+  ///
+  // static final Live4tsdk _internal = Live4tsdk._internal(baseUrl: this.baseUrl);
+
+  // factory Live4tsdk() {
+  //   return _internal;
+  // }
+
+
+  // Live4tsdk._internal({required String baseUrl}) {
+
+  // }
 
   Live4tsdk({required String baseUrl}) {
     // create dio config
@@ -32,6 +55,7 @@ class Live4tsdk {
     calendar = CalendarRepository(httpClient);
     forum = ForumRepository(httpClient);
     message = MessageRepository(httpClient);
+    chat = ChatRepository(httpClient);
   }
 
   bool get ready => true;
