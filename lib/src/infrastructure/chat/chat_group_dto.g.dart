f91871b70fab6b274d7c5931c6da27f75f8e8ad5
Yaicel Torres <yaiceltg@gmail.com>
2021-04-05 15:08:14 -0400

    lib/src/infrastructure/chat/chat_group_dto.g: create

---
+++ lib/src/infrastructure/chat/chat_group_dto.g.dart
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'chat_group_dto.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_ChatGroupDto _$_$_ChatGroupDtoFromJson(Map<String, dynamic> json) {
+  return _$_ChatGroupDto(
+    name: json['name'] as String,
+  );
+}
+
+Map<String, dynamic> _$_$_ChatGroupDtoToJson(_$_ChatGroupDto instance) =>
+    <String, dynamic>{
+      'name': instance.name,
+    };
