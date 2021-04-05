70ffdb4d85417e6e8024303a7a3b32702aa62186
Yaicel Torres <yaiceltg@gmail.com>
2021-04-05 14:10:48 -0400

    lib/src/infrastructure/message/message_dto.g:

--- lib/src/infrastructure/message/message_dto.g.dart
+++ lib/src/infrastructure/message/message_dto.g.dart
@@ -8,29 +8,31 @@ part of 'message_dto.dart';
 
 _$_MessageDto _$_$_MessageDtoFromJson(Map<String, dynamic> json) {
   return _$_MessageDto(
-    id: json['id'] as int,
-    body: json['body'] as String?,
+    id: json['id'] as String,
+    from: AccountDto.fromJson(json['from'] as Map<String, dynamic>),
+    subject: json['subject'] as String,
+    body: json['body'] as String,
     delivered: json['delivered'] == null
         ? null
         : DateTime.parse(json['delivered'] as String),
     read: json['read'] == null ? null : DateTime.parse(json['read'] as String),
-    from: json['from'] as int,
-    to: json['to'] as int,
-    subject: json['subject'] as String?,
-    createdAt: DateTime.parse(json['createdAt'] as String),
-    updatedAt: DateTime.parse(json['updatedAt'] as String),
+    createdAt: json['createdAt'] == null
+        ? null
+        : DateTime.parse(json['createdAt'] as String),
+    updatedAt: json['updatedAt'] == null
+        ? null
+        : DateTime.parse(json['updatedAt'] as String),
   );
 }
 
 Map<String, dynamic> _$_$_MessageDtoToJson(_$_MessageDto instance) =>
     <String, dynamic>{
       'id': instance.id,
+      'from': instance.from,
+      'subject': instance.subject,
       'body': instance.body,
       'delivered': instance.delivered?.toIso8601String(),
       'read': instance.read?.toIso8601String(),
-      'from': instance.from,
-      'to': instance.to,
-      'subject': instance.subject,
-      'createdAt': instance.createdAt.toIso8601String(),
-      'updatedAt': instance.updatedAt.toIso8601String(),
+      'createdAt': instance.createdAt?.toIso8601String(),
+      'updatedAt': instance.updatedAt?.toIso8601String(),
     };
