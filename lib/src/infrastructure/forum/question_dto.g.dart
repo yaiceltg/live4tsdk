ad9c21920a8e9bae43bfb867291d46b050ab88ee
Yaicel Torres <yaiceltg@gmail.com>
2021-04-05 14:10:39 -0400

    lib/src/infrastructure/forum/question_dto.g:

--- lib/src/infrastructure/forum/question_dto.g.dart
+++ lib/src/infrastructure/forum/question_dto.g.dart
@@ -8,20 +8,20 @@ part of 'question_dto.dart';
 
 _$_QuestionDto _$_$_QuestionDtoFromJson(Map<String, dynamic> json) {
   return _$_QuestionDto(
-    id: json['id'] as String?,
-    title: json['title'] as String?,
+    id: json['id'] as String,
+    title: json['title'] as String,
     body: json['body'] as String?,
     views: json['views'] as int?,
     votesCount: json['votesCount'] as int?,
     answerCount: json['answerCount'] as int?,
     bestAnswer: json['bestAnswer'] as String?,
     userId: json['userId'] as int?,
-    createdAt: json['createdAt'] == null
-        ? null
-        : DateTime.parse(json['createdAt'] as String),
+    createdAt: DateTime.parse(json['createdAt'] as String),
     updatedAt: json['updatedAt'] == null
         ? null
         : DateTime.parse(json['updatedAt'] as String),
+    from: AccountDto.fromJson(json['from'] as Map<String, dynamic>),
+    to: AccountDto.fromJson(json['to'] as Map<String, dynamic>),
   );
 }
 
@@ -35,6 +35,8 @@ Map<String, dynamic> _$_$_QuestionDtoToJson(_$_QuestionDto instance) =>
       'answerCount': instance.answerCount,
       'bestAnswer': instance.bestAnswer,
       'userId': instance.userId,
-      'createdAt': instance.createdAt?.toIso8601String(),
+      'createdAt': instance.createdAt.toIso8601String(),
       'updatedAt': instance.updatedAt?.toIso8601String(),
+      'from': instance.from,
+      'to': instance.to,
     };
