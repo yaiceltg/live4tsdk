3c894540a93632afc992e0b20e661d4fc824ef28
Yaicel Torres <yaiceltg@gmail.com>
2021-04-05 10:20:51 -0400

    lib/src/domain/forum/question:

--- lib/src/domain/forum/question.dart
+++ lib/src/domain/forum/question.dart
@@ -1,19 +1,22 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:live4tsdk/src/domain/account/account.dart';
 
 part 'question.freezed.dart';
 
 @freezed
 abstract class Question with _$Question {
   const factory Question({
-    String? id,
-    String? title,
+    required String id,
+    required String title,
     String? body,
     int? views,
     int? votesCount,
     int? answerCount,
     String? bestAnswer,
     int? userId,
-    DateTime? createdAt,
-    DateTime? updatedAt
+    required DateTime createdAt,
+    DateTime? updatedAt,
+    required Account from,
+    required Account to,
   }) = _Question;
 }
