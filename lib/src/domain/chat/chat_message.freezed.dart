ddac8a1963058d40e1b99d2691815f1ef8f25a06
Yaicel Torres <yaiceltg@gmail.com>
2021-04-05 10:21:03 -0400

    lib/src/domain/chat/chat_message.freezed:

--- lib/src/domain/chat/chat_message.freezed.dart
+++ lib/src/domain/chat/chat_message.freezed.dart
@@ -16,8 +16,23 @@ final _privateConstructorUsedError = UnsupportedError(
 class _$ChatMessageTearOff {
   const _$ChatMessageTearOff();
 
-  _Message call() {
-    return const _Message();
+  _Message call(
+      {required int id,
+      required String message,
+      required Account user,
+      required ChatGroup group,
+      required int type,
+      required DateTime createdAt,
+      DateTime? updatedAt}) {
+    return _Message(
+      id: id,
+      message: message,
+      user: user,
+      group: group,
+      type: type,
+      createdAt: createdAt,
+      updatedAt: updatedAt,
+    );
   }
 }
 
@@ -25,13 +40,36 @@ class _$ChatMessageTearOff {
 const $ChatMessage = _$ChatMessageTearOff();
 
 /// @nodoc
-mixin _$ChatMessage {}
+mixin _$ChatMessage {
+  int get id => throw _privateConstructorUsedError;
+  String get message => throw _privateConstructorUsedError;
+  Account get user => throw _privateConstructorUsedError;
+  ChatGroup get group => throw _privateConstructorUsedError;
+  int get type => throw _privateConstructorUsedError;
+  DateTime get createdAt => throw _privateConstructorUsedError;
+  DateTime? get updatedAt => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $ChatMessageCopyWith<ChatMessage> get copyWith =>
+      throw _privateConstructorUsedError;
+}
 
 /// @nodoc
 abstract class $ChatMessageCopyWith<$Res> {
   factory $ChatMessageCopyWith(
           ChatMessage value, $Res Function(ChatMessage) then) =
       _$ChatMessageCopyWithImpl<$Res>;
+  $Res call(
+      {int id,
+      String message,
+      Account user,
+      ChatGroup group,
+      int type,
+      DateTime createdAt,
+      DateTime? updatedAt});
+
+  $AccountCopyWith<$Res> get user;
+  $ChatGroupCopyWith<$Res> get group;
 }
 
 /// @nodoc
@@ -41,12 +79,82 @@ class _$ChatMessageCopyWithImpl<$Res> implements $ChatMessageCopyWith<$Res> {
   final ChatMessage _value;
   // ignore: unused_field
   final $Res Function(ChatMessage) _then;
+
+  @override
+  $Res call({
+    Object? id = freezed,
+    Object? message = freezed,
+    Object? user = freezed,
+    Object? group = freezed,
+    Object? type = freezed,
+    Object? createdAt = freezed,
+    Object? updatedAt = freezed,
+  }) {
+    return _then(_value.copyWith(
+      id: id == freezed
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as int,
+      message: message == freezed
+          ? _value.message
+          : message // ignore: cast_nullable_to_non_nullable
+              as String,
+      user: user == freezed
+          ? _value.user
+          : user // ignore: cast_nullable_to_non_nullable
+              as Account,
+      group: group == freezed
+          ? _value.group
+          : group // ignore: cast_nullable_to_non_nullable
+              as ChatGroup,
+      type: type == freezed
+          ? _value.type
+          : type // ignore: cast_nullable_to_non_nullable
+              as int,
+      createdAt: createdAt == freezed
+          ? _value.createdAt
+          : createdAt // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      updatedAt: updatedAt == freezed
+          ? _value.updatedAt
+          : updatedAt // ignore: cast_nullable_to_non_nullable
+              as DateTime?,
+    ));
+  }
+
+  @override
+  $AccountCopyWith<$Res> get user {
+    return $AccountCopyWith<$Res>(_value.user, (value) {
+      return _then(_value.copyWith(user: value));
+    });
+  }
+
+  @override
+  $ChatGroupCopyWith<$Res> get group {
+    return $ChatGroupCopyWith<$Res>(_value.group, (value) {
+      return _then(_value.copyWith(group: value));
+    });
+  }
 }
 
 /// @nodoc
-abstract class _$MessageCopyWith<$Res> {
+abstract class _$MessageCopyWith<$Res> implements $ChatMessageCopyWith<$Res> {
   factory _$MessageCopyWith(_Message value, $Res Function(_Message) then) =
       __$MessageCopyWithImpl<$Res>;
+  @override
+  $Res call(
+      {int id,
+      String message,
+      Account user,
+      ChatGroup group,
+      int type,
+      DateTime createdAt,
+      DateTime? updatedAt});
+
+  @override
+  $AccountCopyWith<$Res> get user;
+  @override
+  $ChatGroupCopyWith<$Res> get group;
 }
 
 /// @nodoc
@@ -57,27 +165,149 @@ class __$MessageCopyWithImpl<$Res> extends _$ChatMessageCopyWithImpl<$Res>
 
   @override
   _Message get _value => super._value as _Message;
+
+  @override
+  $Res call({
+    Object? id = freezed,
+    Object? message = freezed,
+    Object? user = freezed,
+    Object? group = freezed,
+    Object? type = freezed,
+    Object? createdAt = freezed,
+    Object? updatedAt = freezed,
+  }) {
+    return _then(_Message(
+      id: id == freezed
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as int,
+      message: message == freezed
+          ? _value.message
+          : message // ignore: cast_nullable_to_non_nullable
+              as String,
+      user: user == freezed
+          ? _value.user
+          : user // ignore: cast_nullable_to_non_nullable
+              as Account,
+      group: group == freezed
+          ? _value.group
+          : group // ignore: cast_nullable_to_non_nullable
+              as ChatGroup,
+      type: type == freezed
+          ? _value.type
+          : type // ignore: cast_nullable_to_non_nullable
+              as int,
+      createdAt: createdAt == freezed
+          ? _value.createdAt
+          : createdAt // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      updatedAt: updatedAt == freezed
+          ? _value.updatedAt
+          : updatedAt // ignore: cast_nullable_to_non_nullable
+              as DateTime?,
+    ));
+  }
 }
 
 /// @nodoc
 class _$_Message extends _Message {
-  const _$_Message() : super._();
+  const _$_Message(
+      {required this.id,
+      required this.message,
+      required this.user,
+      required this.group,
+      required this.type,
+      required this.createdAt,
+      this.updatedAt})
+      : super._();
+
+  @override
+  final int id;
+  @override
+  final String message;
+  @override
+  final Account user;
+  @override
+  final ChatGroup group;
+  @override
+  final int type;
+  @override
+  final DateTime createdAt;
+  @override
+  final DateTime? updatedAt;
 
   @override
   String toString() {
-    return 'ChatMessage()';
+    return 'ChatMessage(id: $id, message: $message, user: $user, group: $group, type: $type, createdAt: $createdAt, updatedAt: $updatedAt)';
   }
 
   @override
   bool operator ==(dynamic other) {
-    return identical(this, other) || (other is _Message);
+    return identical(this, other) ||
+        (other is _Message &&
+            (identical(other.id, id) ||
+                const DeepCollectionEquality().equals(other.id, id)) &&
+            (identical(other.message, message) ||
+                const DeepCollectionEquality()
+                    .equals(other.message, message)) &&
+            (identical(other.user, user) ||
+                const DeepCollectionEquality().equals(other.user, user)) &&
+            (identical(other.group, group) ||
+                const DeepCollectionEquality().equals(other.group, group)) &&
+            (identical(other.type, type) ||
+                const DeepCollectionEquality().equals(other.type, type)) &&
+            (identical(other.createdAt, createdAt) ||
+                const DeepCollectionEquality()
+                    .equals(other.createdAt, createdAt)) &&
+            (identical(other.updatedAt, updatedAt) ||
+                const DeepCollectionEquality()
+                    .equals(other.updatedAt, updatedAt)));
   }
 
   @override
-  int get hashCode => runtimeType.hashCode;
+  int get hashCode =>
+      runtimeType.hashCode ^
+      const DeepCollectionEquality().hash(id) ^
+      const DeepCollectionEquality().hash(message) ^
+      const DeepCollectionEquality().hash(user) ^
+      const DeepCollectionEquality().hash(group) ^
+      const DeepCollectionEquality().hash(type) ^
+      const DeepCollectionEquality().hash(createdAt) ^
+      const DeepCollectionEquality().hash(updatedAt);
+
+  @JsonKey(ignore: true)
+  @override
+  _$MessageCopyWith<_Message> get copyWith =>
+      __$MessageCopyWithImpl<_Message>(this, _$identity);
 }
 
 abstract class _Message extends ChatMessage {
-  const factory _Message() = _$_Message;
+  const factory _Message(
+      {required int id,
+      required String message,
+      required Account user,
+      required ChatGroup group,
+      required int type,
+      required DateTime createdAt,
+      DateTime? updatedAt}) = _$_Message;
   const _Message._() : super._();
+
+  @override
+  int get id => throw _privateConstructorUsedError;
+  @override
+  String get message => throw _privateConstructorUsedError;
+  @override
+  Account get user => throw _privateConstructorUsedError;
+  @override
+  ChatGroup get group => throw _privateConstructorUsedError;
+  @override
+  int get type => throw _privateConstructorUsedError;
+  @override
+  DateTime get createdAt => throw _privateConstructorUsedError;
+  @override
+  DateTime? get updatedAt => throw _privateConstructorUsedError;
+  @override
+  @JsonKey(ignore: true)
+  _$MessageCopyWith<_Message> get copyWith =>
+      throw _privateConstructorUsedError;
 }
