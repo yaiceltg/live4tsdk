bbe9297648f6c360d46ad674e9dfb42433da5a77
Yaicel Torres <yaiceltg@gmail.com>
2021-04-05 15:08:13 -0400

    lib/src/infrastructure/chat/chat_message_dto: create

---
+++ lib/src/infrastructure/chat/chat_message_dto.dart
@@ -0,0 +1,49 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:live4tsdk/src/domain/chat/chat_message.dart';
+import 'package:live4tsdk/src/infrastructure/account/account_dto.dart';
+import 'package:live4tsdk/src/infrastructure/chat/chat_group_dto.dart';
+
+part 'chat_message_dto.freezed.dart';
+part 'chat_message_dto.g.dart';
+
+@freezed
+class ChatMessageDto with _$ChatMessageDto {
+  const ChatMessageDto._();
+
+  const factory ChatMessageDto({
+    required int id,
+    required String message,
+    required AccountDto user,
+    required ChatGroupDto group,
+    required int type,
+    required DateTime createdAt,
+    DateTime? updatedAt,
+  }) = _ChatMessageDto;
+
+  factory ChatMessageDto.fromDomain(ChatMessage message) {
+    return ChatMessageDto(
+      id: message.id,
+      message: message.message,
+      user: AccountDto.fromDomain(message.user),
+      group: ChatGroupDto.fromDomain(message.group),
+      type: message.type,
+      createdAt: message.createdAt,
+      updatedAt: message.updatedAt,
+    );
+  }
+
+  ChatMessage toDomain() {
+    return ChatMessage(
+      id: id,
+      message: message,
+      user: user.toDomain(),
+      group: group.toDomain(),
+      type: type,
+      createdAt: createdAt,
+      updatedAt: updatedAt,
+    );
+  }
+
+  factory ChatMessageDto.fromJson(Map<String, dynamic> json) =>
+      _$ChatMessageDtoFromJson(json);
+}
