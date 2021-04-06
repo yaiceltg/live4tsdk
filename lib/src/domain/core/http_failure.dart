2aef764e93254ac07e6262d85fd05b4bbc4ee674
Yaicel Torres <yaiceltg@gmail.com>
2021-04-06 10:22:35 -0400

    lib/src/domain/core/http_failure:

--- lib/src/domain/core/http_failure.dart
+++ lib/src/domain/core/http_failure.dart
@@ -4,6 +4,12 @@ part 'http_failure.freezed.dart';
 
 @freezed
 class HttpFailure with _$HttpFailure {
-  const factory HttpFailure.accessDenied() = _AccessDenied; // 403
-  const factory HttpFailure.sessionExpired() = _SessionExpired; // 401
+  const factory HttpFailure.badRequest() = _BadRequest; // 400 Bad Request
+  const factory HttpFailure.unauthorized() = _Unauthorized; // 401 Unauthorized
+  const factory HttpFailure.forbidden() = _Forbidden; // 403 Forbidden
+  const factory HttpFailure.notFound() = _NotFound; // 404 Not Found
+  const factory HttpFailure.internal() = _Internal; // 500 Internal Server Error
+  const factory HttpFailure.badGateway() = _BadGateway; // 502 Bad Gateway
+  const factory HttpFailure.serviceUnavailable() = _ServiceUnavailable; // 503 Service Unavailable
+  const factory HttpFailure.gatewayTimeout() = _GatewayTimeout; // 504 Gateway Timeout
 }
