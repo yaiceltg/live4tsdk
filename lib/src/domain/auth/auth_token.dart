3432bc5bba8fce7e1218ea8e33648fb234b37ebd
Yaicel Torres <yaiceltg@gmail.com>
2021-04-05 22:36:14 -0400

    lib/src/domain/auth/auth_token:

--- lib/src/domain/auth/auth_token.dart
+++ lib/src/domain/auth/auth_token.dart
@@ -4,7 +4,7 @@ import 'package:live4tsdk/src/domain/auth/value_objects.dart';
 part 'auth_token.freezed.dart';
 
 @freezed
-abstract class AuthToken with _$AuthToken {
+class AuthToken with _$AuthToken {
   const factory AuthToken({
     required AccessToken accessToken,
   }) = _AuthToken;
