e12a813f86fc2b4ff33306c008ba4d7b22bb8a3c
Yaicel Torres <yaiceltg@gmail.com>
2021-04-06 12:48:01 -0400

    example/chat/send_messages: create

---
+++ example/chat/send_messages.dart
@@ -0,0 +1,46 @@
+import 'package:live4tsdk/live4tsdk.dart';
+
+import '../sdk_instance.dart';
+
+void main() async {
+  final sdk = Live4tsdk.instance;
+
+  // login
+  await login();
+
+  //
+  final result = await sdk.chat.sendMessage(
+    message: "hola desde el sdk",
+    roomId: '4',
+    type: 1,
+  );
+
+  final msg = result.fold(
+    (error) => error.map(
+      http: (httpError) => httpError.error.map(
+        badRequest: (value) => 'Bad Request',
+        unauthorized: (value) => 'Unauthorized',
+        forbidden: (value) => 'Forbidden',
+        notFound: (value) => 'Not Found',
+        internal: (value) => 'Internal',
+        badGateway: (value) => 'BadGateway',
+        serviceUnavailable: (value) => 'Service Unavailable',
+        gatewayTimeout: (value) => 'Gateway Timeout',
+      ),
+      serverError: (_) => 'Server error' // deprecated
+    ), (r) => 'Success'
+  );
+
+  // httpError.error.map(
+  //       badRequest: () => 'Bad Request',
+  //       unauthorized: () => 'Unauthorized',
+  //       forbidden: () => 'Forbidden',
+  //       notFound: () => 'Not Found',
+  //       internal: () => 'Internal',
+  //       badGateway: () => 'BadGateway',
+  //       serviceUnavailable: () => 'Service Unavailable',
+  //       gatewayTimeout: () => 'Gateway Timeout',
+  //     )
+
+  print(msg);
+}
