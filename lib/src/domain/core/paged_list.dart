8d165886cc4b900cdbbaf54191738192ab7e5907
Yaicel Torres <yaiceltg@gmail.com>
2021-04-05 22:35:58 -0400

    lib/src/domain/core/paged_list:

--- lib/src/domain/core/paged_list.dart
+++ lib/src/domain/core/paged_list.dart
@@ -3,7 +3,7 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 part 'paged_list.freezed.dart';
 
 @freezed
-abstract class PagedList<T> implements _$PagedList<T> {
+class PagedList<T> with _$PagedList<T> {
   const PagedList._();
 
   const factory PagedList({
