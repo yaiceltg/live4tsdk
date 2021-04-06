794a590d841cb2c03471560a4a658f2cd1c973f2
Yaicel Torres <yaiceltg@gmail.com>
2021-04-05 22:06:40 -0400

    lib/src/live4tsdk_base:

--- lib/src/live4tsdk_base.dart
+++ lib/src/live4tsdk_base.dart
@@ -1,4 +1,7 @@
+import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
+import 'package:live4tsdk/src/domain/auth/auth_failure.dart';
+import 'package:live4tsdk/src/domain/auth/value_objects.dart';
 import 'package:live4tsdk/src/infrastructure/account/account_repository.dart';
 import 'package:live4tsdk/src/infrastructure/auth/auth_repository.dart';
 import 'package:live4tsdk/src/infrastructure/calendar/calendar_repository.dart';
@@ -7,15 +10,20 @@ import 'package:live4tsdk/src/infrastructure/forum/forum_repository.dart';
 import 'package:live4tsdk/src/infrastructure/message/message_repository.dart';
 
 class Live4tsdk {
+  ///
+  /// Shared data
+  ///
+  // final Account currentUser;
+  // final String authToken;
 
   ///
   /// Dio http client config
   ///
-  // final String baseUrl;
+  final String baseUrl = 'http://164.90.157.86:3000/api';
   Dio? httpClient;
 
   ///
-  /// Api repository
+  /// Sdk modules
   ///
   late AccountRepository account;
   late AuthRepository auth;
@@ -29,18 +37,9 @@ class Live4tsdk {
   /// _cache is library-private, thanks to
   /// the _ in front of its name.
   ///
-  // static final Live4tsdk _internal = Live4tsdk._internal(baseUrl: this.baseUrl);
+  static final Live4tsdk instance = Live4tsdk._internal();
 
-  // factory Live4tsdk() {
-  //   return _internal;
-  // }
-
-
-  // Live4tsdk._internal({required String baseUrl}) {
-
-  // }
-
-  Live4tsdk({required String baseUrl}) {
+  Live4tsdk._internal() {
     // create dio config
     final _options = BaseOptions(
       baseUrl: baseUrl,
@@ -64,5 +63,45 @@ class Live4tsdk {
     httpClient!.options.headers.addAll({
       'Authorization': 'Bearer $token'
     });
+  }
+
+  ///
+  /// Shared functions
+  ///
+  Future<Either<AuthFailure, Unit>> signInWithEmailAndPassword({
+    required String emailAddress,
+    required String password
+  }) async {
+    // call auth method of auth module
+    final _tokenOrError = await auth.signInWithEmailAndPassword(
+      emailAddress: EmailAddress(emailAddress),
+      password:  Password(password)
+    );
+
+    // analyze response result
+    _tokenOrError.fold((error) {
+      // logguer error
+      final _erorName = error.maybeMap(
+        serverError: (_) => "Server error ",
+        invalidEmailAndPasswordCombination: (_) => "Usuario o contraseña incorrecta",
+        userNotFound: (_) => "User not found",
+        orElse: () => "Ninguno"
+      );
+
+      print(_erorName);
+
+      return left(error);
+    }, (t) async {
+      // get token value
+      final _token = t.accessToken.getOrCrash();
+
+      // add auth token bearer for all request
+      addHeaderAuthorization(_token);
+
+      // decoded token and store
+      return right(unit);
+    });
+
+    return left(AuthFailure.serverError());
   }
 }
