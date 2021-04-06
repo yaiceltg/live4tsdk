0f4f7ab88699937b94ae9fa8f1bd31679e5f8182
Yaicel Torres <yaiceltg@gmail.com>
2021-04-06 10:10:16 -0400

    lib/src/domain/message/message_failure:

--- lib/src/domain/message/message_failure.dart
+++ lib/src/domain/message/message_failure.dart
@@ -1,8 +1,10 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:live4tsdk/src/domain/core/http_failure.dart';
 
 part 'message_failure.freezed.dart';
 
 @freezed
 class MessageFailure with _$MessageFailure {
-  const factory MessageFailure.serverError() = _EerverError;
+  const factory MessageFailure.httpError(HttpFailure failure) = _HttpError;
+  const factory MessageFailure.serverError() = _ServerError;
 }
