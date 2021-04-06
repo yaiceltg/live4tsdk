37bd45ad4dc7b67e3a5163319f7c85cb66664c9c
Yaicel Torres <yaiceltg@gmail.com>
2021-04-05 22:36:04 -0400

    lib/src/domain/calendar/i_calendar_repository:

--- lib/src/domain/calendar/i_calendar_repository.dart
+++ lib/src/domain/calendar/i_calendar_repository.dart
@@ -1,32 +1,31 @@
 import 'package:dartz/dartz.dart';
 import 'package:live4tsdk/src/domain/calendar/calendar_event.dart';
 import 'package:live4tsdk/src/domain/calendar/calendar_failure.dart';
-import 'package:live4tsdk/src/domain/core/paged_list.dart';
 
 abstract class ICalendarRepository {
   ///
   /// list calendar events
   ///
-  Future<Either<CalendarFailure, PagedList<CalendarEvent>>> fetchEvents();
+  Future<Either<CalendarFailure, List<CalendarEvent>>> fetchEvents();
 
-  ///
-  /// create calendar event
-  ///
-  Future<Either<CalendarFailure, Unit>> createEvent({
-    required String title
-  });
+  // ///
+  // /// create calendar event
+  // ///
+  // Future<Either<CalendarFailure, Unit>> createEvent({
+  //   required String title
+  // });
 
-  ///
-  /// update calendar event
-  ///
-  Future<Either<CalendarFailure, Unit>> updateEvent({
-    required CalendarEvent event,
-  });
+  // ///
+  // /// update calendar event
+  // ///
+  // Future<Either<CalendarFailure, Unit>> updateEvent({
+  //   required CalendarEvent event,
+  // });
 
-  ///
-  /// delete calendar event
-  ///
-  Future<Either<CalendarFailure, Unit>> deleteEvent({
-    required CalendarEvent event
-  });
+  // ///
+  // /// delete calendar event
+  // ///
+  // Future<Either<CalendarFailure, Unit>> deleteEvent({
+  //   required CalendarEvent event
+  // });
 }
