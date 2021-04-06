650202295d5e6f1a3c13a5350ea946e3b705ca24
Yaicel Torres <yaiceltg@gmail.com>
2021-04-05 22:36:17 -0400

    lib/src/domain/auth/auth_failure:

--- lib/src/domain/auth/auth_failure.dart
+++ lib/src/domain/auth/auth_failure.dart
@@ -3,7 +3,7 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 part 'auth_failure.freezed.dart';
 
 @freezed
-abstract class AuthFailure with _$AuthFailure {
+class AuthFailure with _$AuthFailure {
   const factory AuthFailure.cancelledByUser() = _CancelledByUser;
   const factory AuthFailure.serverError() = _ServerError;
   const factory AuthFailure.tokenNotFound() = _TokenNotFound;
