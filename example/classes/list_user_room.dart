de9fa96fa33841dc1c48885ec8e5a16d0ddc7581
Yaicel Torres <yaiceltg@gmail.com>
2021-06-15 22:42:44 -0400

    added examples

---
+++ example/classes/list_user_room.dart
@@ -0,0 +1,24 @@
+import 'package:live4tsdk/live4tsdk.dart';
+
+import '../sdk_instance.dart';
+
+void main() async {
+  // start session
+  await login();
+
+  final live4tsdk = Live4tsdk.instance;
+
+  // get to account service
+  final failureOrQuestions = await live4tsdk.classes.fetchUserRoom();
+
+  failureOrQuestions.fold((error) {
+    final _eName = error.maybeMap(
+      unknown: (_) => "Error desconocido",
+      orElse: () => "Error"
+    );
+
+    print(_eName);
+  }, (areas) {
+    print(areas);
+  });
+}
