0bf4662754ac435df7aba69762e7470b331b5f5a
Yaicel Torres <yaiceltg@gmail.com>
2021-10-28 02:20:27 -0400

    remove unused method

--- lib/src/infrastructure/academic/scheduler.dart
+++ lib/src/infrastructure/academic/scheduler.dart
@@ -1,16 +1,6 @@
 part of 'academic_repository.dart';
 
 extension SchedulerRepository on AcademicRepository {
-  Future<Either<dynamic, HttpFailure>> fetchUserClassAndMateria() async {
-    try {
-      // call api service
-      final _response = await _httpClient.get(''); // TODO:
-
-      return right(_response.data);
-    } catch (e) {
-      return left(HttpFailure.internal());
-    }
-  }
 
   Future<Either<dynamic, HttpFailure>> saveGeneral({
     required List<Achievement> achievements,
